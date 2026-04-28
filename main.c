05c964bbab6a7466688556f39426563e2ccba487
Zachary White <sharperaim@gmail.com>
2026-04-28 13:29:07 -0600

    got some dependencies all set up with my toolchain

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
-    return 0;
+    int* systemMemoryStart = malloc(512); // declare the Chip-8 RAM space
+    printf("%p", systemMemoryStart);
+
+
+    
+    free(systemMemoryStart); // clean everything up at the end
 }
