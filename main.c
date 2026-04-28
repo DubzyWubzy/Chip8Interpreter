4d94d273f4ffb00e4d4ad3e583539cd276299178
Zachary White <sharperaim@gmail.com>
2026-04-28 13:39:56 -0600

    started on making some memory structures to be tested

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int STACK_SIZE = 16; // for 16 2-byte entires
+
+struct callStack
+{
+    uint16_t stack[16];// array where each item is 16 bits
+    // top
+    // capacity
+};
+
+
 int main(void)
 {
     int* systemMemoryStart = malloc(512); // declare the Chip-8 RAM space
     printf("%p", systemMemoryStart);
 
 
-    
+
+
     free(systemMemoryStart); // clean everything up at the end
 }
